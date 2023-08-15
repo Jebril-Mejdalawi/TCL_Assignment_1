d2f7f01d72b6701d52cfc375506bb1acb9b7dee2
JEBREEL MAJDALWIEH <jemajdalwieh18@cit.just.edu.jo>
2023-08-15 07:55:13 +0300

    try lsort to make sure list is correctly parsed

--- main.tcl
+++ main.tcl
@@ -5,12 +5,23 @@
 
 #set numlist {split [gets stdin] " "}
 
+puts "\v\vplease enter a list of the numbers from 0 to 9 but in any order you want,\nI will try to sort it as quickly as possible:\v"
 set input [gets stdin]
 set numlist [split $input " "]
 
-puts "list is :"
+puts "list before sorting :"
 set i 1
 foreach li $numlist {
-    puts " $i : $li"
+    puts "list of index $i : $li"
+    incr i
+}
+puts "\v-----------------------------------------------------------------------\v"
+#!trying to use lsort predefined command to make sure the list is correctly parsed.
+set sortedList [lsort $numlist]  
+
+puts "list After sorting :"
+set i 1
+foreach li $sortedList {
+    puts "list of index $i : $li"
     incr i
 }
