6e835d3b676b990332ac8da2bb134fc97fac9db7
JEBREEL MAJDALWIEH <jemajdalwieh18@cit.just.edu.jo>
2023-08-15 08:37:07 +0300

    implement the mergeSort recursive procedure

--- main.tcl
+++ main.tcl
@@ -2,11 +2,28 @@
 
 
 
-proc merge {numList} {
-    
+proc mergeSort {numList} {
+    set length [llength $numList]
+    if { $length <= 1} {
+        #return $numList
+        return 0
+    }
+
+    set mid [expr $length / 2]
+    set left [lrange $numList 0 [expr $mid - 1]]
+    set right [lrange $numList $mid $length-1]
+    return merge mergeSort $left mergeSort $right
+
+    #testing values
+    if 0 {
+    puts $mid
+    puts $left
+    puts $right
+    }
+  
 }
 
-proc mergeSort { left , right} {
+proc merge { left , right} {
     
 }
 
@@ -15,22 +32,17 @@ proc mergeSort { left , right} {
 if {1} {
 puts "\v\vplease enter a list of the numbers from 0 to 9 but in any order you want,\nI will try to sort it as quickly as possible:\v"
 set input [gets stdin]
-set numlist [split $input " "]
+set nList [split $input " "]
 
 puts "list before sorting :"
 set i 1
-foreach li $numlist {
+foreach li $nList {
     puts "list of index $i : $li"
     incr i
 }
 puts "\v-----------------------------------------------------------------------\v"
-#!trying to use lsort predefined command to make sure the list is correctly parsed.
-set sortedList [lsort $numlist]  
 
-puts "list After sorting :"
-set i 1
-foreach li $sortedList {
-    puts "list of index $i : $li"
-    incr i
-}
+
+puts [mergeSort $nList]
+
 }
